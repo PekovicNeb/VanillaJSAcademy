b0c3ab3173979fa519fb9e544d20957d60d36a65
Neb Pekovic <58142968+PekovicNeb@users.noreply.github.com>
2020-10-13 14:53:06 +0100

    Add files via upload

---
+++ script/vanillaJS.js
@@ -0,0 +1,37 @@
+// my code goes here
+//
+//Variables
+//
+var password = document.querySelector('#password');  
+var toggle = document.querySelector('#show-password');  
+
+//
+//Methods
+//
+
+/**
+ * Handle click events
+ * @param {Event} event The event object
+ */
+var clickHandler = function(event){
+    //console.log(event.target);
+    //Only run if show-password checkbox is clicked
+    var eventTarget = event.target.getAttribute('id');
+
+    if(eventTarget!="toggle") return;
+    //console.log(eventTarget);
+    //console.log(event.target.checked);
+    if(event.target.checked){
+      password.type = 'text';
+    } else {
+       password.type = 'password';
+    }
+
+};
+
+//Event Listeners
+
+//Listen for the clicks in DOM
+//event gets passed implicitly
+document.addEventListener('click',clickHandler);
+
