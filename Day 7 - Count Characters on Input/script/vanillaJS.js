192e1387db4f1afef1b401131176ddb3952a49ad
PekovicNeb <58142968+PekovicNeb@users.noreply.github.com>
2020-10-20 22:26:12 +0100

    Day 7 - Count Characters on Input Event
    Count Characters on Input Event

---
+++ Day 7 - Count Characters on Input/script/vanillaJS.js
@@ -0,0 +1,104 @@
+// my code goes here
+//
+// Polyfills
+//
+/**
+ * Element.matches() polyfill (simple version)
+ * https://developer.mozilla.org/en-US/docs/Web/API/Element/matches#Polyfill
+ */
+if (!Element.prototype.matches) {
+	Element.prototype.matches = Element.prototype.msMatchesSelector || Element.prototype.webkitMatchesSelector;
+}
+
+//
+// Classes
+//
+
+class Password {
+  constructor(pass){
+    this.pass=pass;
+  }
+//Get validation
+  get getStatus(){
+    return this.checkValidation();
+  }
+
+  checkValidation(){
+  if(this.pass.length < 6){
+      return "Length too short";
+    }else {
+      return "Validation passed";
+    }
+  }
+}
+
+//
+//Variables
+//
+//NodeList
+var passwords = [];
+//document.querySelectorAll('.password');  
+//get NodeList into an array object to be able to use all array methods
+var passArray = [];
+//Array.prototype.slice.call(passwords);
+
+var newPasswordType = "";
+
+var toggle = document.querySelector('#show-password');  
+var securityAnswer = document.querySelector('#textSecurityAnswer');  
+var characterCount = document.querySelector('#character-count');
+//
+//Methods
+//
+
+var showPassword = function(event){
+  if(!event.target.matches('[data-pw-toggle]')) return;
+  passwords = passwords = document.querySelectorAll(event.target.getAttribute('data-pw-toggle'));
+  passArray = Array.prototype.slice.call(passwords);
+
+  if(event.target.checked){
+    newPasswordType = 'text';
+  } else {
+    newPasswordType = 'password';
+  } 
+  changePasswordType(newPasswordType);
+}
+
+var changePasswordType = function(newType){
+  passArray.forEach(function(pass){
+    pass.type=newType;
+    console.log(pass.value);
+    p = new Password(pass.value);
+    console.log(p.getStatus);
+},false);
+}
+
+/**
+ * Handle click events
+ * @param {Event} event The event object
+ */
+var clickHandler = function(event){
+    showPassword(event);
+};
+
+/**
+ * Handle key up events
+ * @param {Event} event The event object
+ */
+var inputHandler = function(event){
+  countLetters(event.target.value);
+};
+
+var countLetters = function(answer){
+  characterCount.textContent = answer.length;
+};
+
+
+//Event Listeners
+
+//Listen for the clicks in DOM
+//event gets passed implicitly
+securityAnswer.addEventListener('input',inputHandler);
+document.addEventListener('click',clickHandler);
+
+
